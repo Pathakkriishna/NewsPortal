25508533f379c0e6173eb49affcfb68a005b9325
Pathkkriishna <kriishna.pathak@gmail.com>
2022-07-12 21:12:14 +0545

    initial commit

---
+++ news-master/DMS/DMS.UI/Controllers/adminController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DMS.Controllers
+{
+    
+    public class adminController : Controller
+    {
+        // GET: admin
+       
+        public ActionResult adindex()
+        {
+            return View();
+        }
+    }
+}
